160f9e487d60a23c9cafebf785641372b7f2a6f9
Sachin negi <san.negi.77777@gmail.com>
2020-07-24 23:19:06 +0530

    build folder

---
+++ build/precache-manifest.92762b9f28711b877d9028dad0a86a53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827e51e5119dc94d4d00c7187ec5ea57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "126119c5a7dde770cfc4",
+    "url": "/static/css/2.46117652.chunk.css"
+  },
+  {
+    "revision": "a7a1b969b945beee1943",
+    "url": "/static/css/main.88eb895f.chunk.css"
+  },
+  {
+    "revision": "126119c5a7dde770cfc4",
+    "url": "/static/js/2.04faaa00.chunk.js"
+  },
+  {
+    "revision": "c3e749c851fbe61ff1294c797ef19a6b",
+    "url": "/static/js/2.04faaa00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a1b969b945beee1943",
+    "url": "/static/js/main.b2bcafd4.chunk.js"
+  },
+  {
+    "revision": "907ea6149720ae1c9910",
+    "url": "/static/js/runtime-main.f3670a13.js"
+  },
+  {
+    "revision": "0fddd2aeaee74e0fc87ffdecdfc1a465",
+    "url": "/static/media/AdminProfile.0fddd2ae.jpeg"
+  }
+]);
